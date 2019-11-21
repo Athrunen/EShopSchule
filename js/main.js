a88771466e3ebb92cad3492921128b18a13db4c9
Malte Vrampe <malte.vrampe@Raum92-11.SCHULE>
2019-11-21 09:21:11 +0100

    Done some shit

--- js/main.js
+++ js/main.js
@@ -1,31 +1,53 @@
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
 window.App = new Vue({
     el: '#grid',
     data () {
         return {
             products: {
                 "ccc": {
-                    name: "aaa",
+                    name: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
                     type: 2,
-                    image: "images/Instrumente/Gitarren/Verstärker/Line6 Spider V 20 MkII.jpg",
+                    image: "'images/Instrumente/Gitarren/Verstärker/Line6 Spider V 20 MkII.jpg'",
                     price: 0,
+                    desc: "dkjdgsjhsdfhdsjbkljhf",
                 },
                 "ccd": {
-                    name: "aab",
+                    name: "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
                     type: 1,
-                    image: "images/Instrumente/Gitarren/Verstärker/Line6 Spider V 30 MkII.jpg",
-                    price: 0,
+                    image: "'images/Instrumente/Gitarren/Verstärker/Line6 Spider V 30 MkII.jpg'",
+                    price: 18.88,
+                    desc: "lkldasklasklklaskla",
                 },
                 "cdd": {
                     name: "abb",
                     type: 0,
-                    image: "images/Instrumente/Gitarren/Verstärker/Line6 Spider V 120 MkII.jpg",
+                    image: "'images/Instrumente/Gitarren/Verstärker/Line6 Spider V 120 MkII.jpg'",
                     price: 0,
+                    desc: "qewuiqwzequwizeuqizweiuzwhbc",
                 },
                 "cda": {
                     name: "dbb",
                     type: 0,
-                    image: "images/Instrumente/Gitarren/Zubehör/Plektren/Harley Benton Nylon Player Pick Set Mixed.jpg",
+                    image: "'images/Instrumente/Gitarren/Zubehör/Plektren/Harley Benton Nylon Player Pick Set Mixed.jpg'",
                     price: 0,
+                    desc: "gftdretzdoizöovcö",
+                },
+                "ggh": {
+                    name: "dbb",
+                    type: 0,
+                    image: "'images/Instrumente/Gitarren/Zubehör/Plektren/Harley Benton Nylon Player Pick Set Mixed.jpg'",
+                    price: 0,
+                    desc: "gftdretzdoizöovcö",
+                },
+                "dcs": {
+                    name: "dbb",
+                    type: 0,
+                    image: "'images/Instrumente/Gitarren/Zubehör/Plektren/Harley Benton Nylon Player Pick Set Mixed.jpg'",
+                    price: 0,
+                    desc: "gftdretzdoizöovcö",
                 },
             },
             filters: [],
@@ -37,11 +59,9 @@ window.App = new Vue({
     },
     watch: {
         search: function () {
-            console.log("a")
             this.filteredProducts()
         },
         filters: function () {
-            console.log("b")
             this.filteredProducts()
         }
     },
@@ -51,7 +71,7 @@ window.App = new Vue({
     methods: {
         filteredProducts: function () {
             if (this.filters.length == 0 && this.search == "")
-                Object.assign(this.filtered, this.products)
+                return Object.assign(this.filtered, this.products)
             var reduced = {}
             Object.keys(this.products).forEach(key => {
                 if (this.filters.includes(this.products[key].type) || (this.search != "" && this.products[key].name.includes(this.search)))
